fedac240d7826cf346db6789cf0cfe6ea5827b46
sagather <samantha.agather@hotmail.com>
2017-05-25 17:41:46 -0700

    A_Component stubbed out

--- src/s17cs350task1/A_Component.java
+++ src/s17cs350task1/A_Component.java
@@ -1,5 +1,8 @@
 package s17cs350task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bcxtr on 5/25/2017.
  */
@@ -46,5 +49,88 @@ public abstract class A_Component {
         return false;
     }
 
-    public abstract String export(A_Exporter.export);
+    public abstract String export(A_Exporter exporter);
+
+    abstract BoundingBox generateBoundingBoxAll();
+
+    abstract BoundingBox generateBoundingBoxSelf();
+
+    abstract List<List<Point3D>> generateFramesAll();
+
+    abstract List<Point3D> generateFrameSelf();
+
+    public Point3D getAbsoluteCenterPosition(){
+
+        return new Point3D(0,0,0);
+
+    }
+
+    public int getChildCount(){
+
+        return 0;
+    }
+
+    public List<A_Component> getChildren(){
+
+        return new ArrayList<A_Component>();
+    }
+
+    public List<Connector> getConnectorsToChildren(){
+
+        return new ArrayList<>();
+    }
+
+    public Connector getConnectorToParent(){
+
+        return new Connector(new Box("", new Dimension3D(0,0,0)), new Point3D(0,0,0));
+    }
+
+    public int getDescendantCount(){
+
+        return 0;
+    }
+
+    public List<A_Component> getDescendants(){
+
+        return new ArrayList<>();
+    }
+
+    public String getID(){
+
+        return "";
+    }
+
+    List<A_Component> getSubtree(){
+
+        return new ArrayList<>();
+    }
+
+    List<A_Component> getTree(){
+
+        return new ArrayList<>();
+    }
+
+    public boolean hasConnectorToParent(){
+
+        return false;
+    }
+
+    public int hashCode(){
+
+        return 0;
+    }
+
+    public boolean isRoot(){
+
+        return this.root;
+    }
+
+    void setConnectorToParent(Connector connector){
+
+    }
+
+    public String toString(){
+
+        return "";
+    }
 }
